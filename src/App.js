09499f9e600f3ddd78186e6da6b13321affb2fb3
lucaslovaglio <lucas.lovaglio@ing.austral.edu.ar>
2024-05-28 20:22:59 -0300

    Update: styles updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import {useEffect, useState} from "react";
-import {getAllItems, postOrder} from "./service";
+import { useEffect, useState } from "react";
+import { getAllItems, postOrder } from "./service";
 
 function App() {
-
-    // State to hold the products in the store
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+    const [order, setOrder] = useState(undefined);
 
     const fetchItems = async () => {
         getAllItems().then(items => setProducts(items));
@@ -15,12 +15,6 @@ function App() {
         fetchItems();
     }, []);
 
-    // State to hold the items in the shopping cart
-    const [cart, setCart] = useState([]);
-
-    const [order, setOrder] = useState(undefined);
-
-    // Function to add a product to the cart
     const addToCart = (product) => {
         for (let i = 0; i < cart.length; i++) {
             if (cart[i].id === product.id) {
@@ -33,46 +27,47 @@ function App() {
         setCart([...cart, product]);
     };
 
-    // Function to handle making an order
     const makeOrder = async () => {
-        // For demonstration, just logging the order details to the console
-        const order = await postOrder({items: cart});
-        // Clearing the cart after placing the order
-        setOrder(order)
+        const order = await postOrder({ items: cart });
+        setOrder(order);
         setCart([]);
     };
 
     return (
-        <div>
-            <h1>Online Store</h1>
-            <h2>Products</h2>
-            <ul>
-                {products.length > 0 && products.map((product) => (
-                    <li key={product.id}>
-                        {product.name} - ${product.price}
-                        <button onClick={() => addToCart(product)}>Add to Cart</button>
-                    </li>
-                ))}
-            </ul>
-            <h2>Shopping Cart</h2>
-            <ul>
-                {cart.map((item, index) => (
-                    <li key={index}>
-                        {item.name} - ${item.price} - Qty: {item.quantity}
-                    </li>
-                ))}
-            </ul>
-            {cart.length > 0 && (
-                <button onClick={makeOrder}>Place Order</button>
-            )}
-
-            {order && (
-                <div>
-                    <h2>Order Details</h2>
-                    <p>Order ID: {order.id}</p>
-                    <p>Order Status: {order.status}</p>
-                </div>
-            )}
+        <div className="App">
+            <header className="App-header">
+                <h1>Online Store</h1>
+            </header>
+            <div>
+                <h2>Products</h2>
+                <ul>
+                    {products.length > 0 && products.map((product) => (
+                        <li key={product.id}>
+                            {product.name} - ${product.price}
+                            <br/>
+                            <button onClick={() => addToCart(product)}>Add to Cart</button>
+                        </li>
+                    ))}
+                </ul>
+                <h2>Shopping Cart</h2>
+                <ul>
+                    {cart.map((item, index) => (
+                        <li key={index}>
+                            {item.name} - ${item.price} - Qty: {item.quantity}
+                        </li>
+                    ))}
+                </ul>
+                {cart.length > 0 && (
+                    <button onClick={makeOrder}>Place Order</button>
+                )}
+                {order && (
+                    <div className="order-details">
+                        <h2>Order Details</h2>
+                        <p>Order ID: {order.id}</p>
+                        <p>Order Status: {order.status}</p>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
